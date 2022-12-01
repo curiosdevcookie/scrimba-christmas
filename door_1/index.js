678c8b24a51a14066b4ea08cd472ca973f810e52
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-12-01 10:40:29 +0100

    Add 1st door.

---
+++ door_1/index.js
@@ -0,0 +1,10 @@
+const string = "I'm almost out of coffee"
+
+function panic(string) {
+  const newString = string.toUpperCase().replaceAll(' ', ' 😱 ');
+  return newString;
+}
+
+console.log(panic(string));
+
+document.body.innerHTML = panic(string);
