ab14ef3fed42c98abe038de9da23f82ef4386d8a
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2022-12-05 10:03:23 +0100

    Add solution to door 5.

---
+++ door_5/index.js
@@ -0,0 +1,46 @@
+import products from "./data.js";
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+function getSaleItems(products) {
+
+  const shoppingCart = products.filter((product) => product.type === "sweet").map((product) => {
+    return {
+      item: product.item,
+      price: product.price
+    }
+  });
+
+  return shoppingCart;
+
+};
+console.log(getSaleItems(products));
+
+
+//Adding to the DOM;
+
+const string = JSON.stringify(getSaleItems(products));
+
+const priceParagraph = document.createElement('p')
+
+const priceNode = document.createTextNode(string);
+
+document.getElementById('products').appendChild(priceNode);
+
+
+
