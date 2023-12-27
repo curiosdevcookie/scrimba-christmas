302f3f321dc4d176cd5d02271d259697df0eb78c
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2023-12-27 09:12:50 +0100

    Set localStorage for people list.

--- 2023/door_22/index.js
+++ 2023/door_22/index.js
@@ -1,52 +1,60 @@
-let people = []
+let people = JSON.parse(localStorage.getItem("people")) || [];
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const peopleListEl = document.getElementById("people-list")
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    if (inputValue) {
-        people.push(inputValue)
-        
-        clearInputFieldEl()
-        
-        renderList(people)
-    }
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value
+
+
+  if (inputValue) {
+    people.push(inputValue)
+    localStorage.setItem("people", JSON.stringify(people));
+
+    console.log(localStorage.getItem("people"));
+
+    clearInputFieldEl()
+
+    renderList(people)
+  }
 })
 
 function renderList(array) {
-    clearPeopleListEl()
-    
-    for (let i = 0; i < array.length; i++) {
-        let currentPerson = array[i]
-        
-        appendPersonToPeopleListEl(currentPerson)
-    }
+  clearPeopleListEl()
+
+  for (let i = 0; i < array.length; i++) {
+    let currentPerson = array[i]
+
+    appendPersonToPeopleListEl(currentPerson)
+  }
 }
 
 function clearPeopleListEl() {
-    peopleListEl.innerHTML = ""
+  peopleListEl.innerHTML = ""
 }
 
 function clearInputFieldEl() {
-    inputFieldEl.value = ""
+  inputFieldEl.value = ""
 }
 
 function appendPersonToPeopleListEl(person) {
-    
-    let newEl = document.createElement("li")
-    
-    newEl.textContent = person
-    
-    newEl.addEventListener("dblclick", function() {
-        let index = people.indexOf(person)
 
-        people.splice(index, 1)
-            
-        renderList(people)
-    })
-    
-    peopleListEl.append(newEl)
+  let newEl = document.createElement("li")
+
+  newEl.textContent = person
+
+  newEl.addEventListener("dblclick", function () {
+    let index = people.indexOf(person)
+
+    people.splice(index, 1)
+    localStorage.setItem("people", JSON.stringify(people));
+
+    renderList(people)
+  })
+
+  peopleListEl.append(newEl)
 }
+
+// Very important to render the list on page load:
+renderList(people);
