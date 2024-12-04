cf1ee350362a79b10a7bc11cd0540eecd87fe342
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-12-04 17:14:36 +0100

    Solution to day 2.

--- 2024/door_02/index.js
+++ 2024/door_02/index.js
@@ -1,16 +1,33 @@
 const calendarContainer = document.getElementById('calendar');
 
 for (let i = 1; i <= 24; i++) {
-  let box = document.createElement('li');
-  box.classList.add('calendar-box');
+  let box = document.createElement('article');
+  box.classList.add('card');
+
+  let card = document.createElement('div');
+  card.classList.add('card-inner');
+
+  let cardFront = document.createElement('div');
+  cardFront.classList.add('card-front');
+
+  let cardBack = document.createElement('div');
+  cardBack.classList.add('card-backside');
+
   let number = document.createElement('p');
-  number.innerHTML = i;
+  number.classList.add('number');
+  number.textContent = i;
+
   const icon = document.createElement('i');
   icon.classList.add('fas', 'fa-gift');
-  let description = document.createElement('p');
-  description.innerHTML = "Open me!";
-  box.appendChild(number);
-  box.appendChild(icon);
-  box.appendChild(description);
+
+  // Append elements in the correct order
+  cardBack.appendChild(number);
+  cardBack.appendChild(icon);
+
+  card.appendChild(cardFront);
+  card.appendChild(cardBack);
+
+  box.appendChild(card);
+
   calendarContainer.appendChild(box);
 }
