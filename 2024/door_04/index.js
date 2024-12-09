94ac04f09fecc2eb532c5431b679825d1b07b21c
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-12-09 07:23:48 +0100

    Solution to day 4.

--- 2024/door_04/index.js
+++ 2024/door_04/index.js
@@ -1,37 +1,97 @@
-/*
-You are going to build an app that challenges players to identify a Christmas Movie from some emoji 🍿 🎅 🎬. The players will have 3 guesses per movie.
-
-For example, the emoji 🌇 💣 👮 ✈️ ️🔫  represent the film “Die Hard”, which everyone knows is the best Christmas movie of all time.
-
-In data.js you have an array of Christmas movies with emoji and text for aria labels.
-
-Your task is to build an app that meets these criteria:
-
-- The app should present the player with a set of emoji selected at random from the array in data.js. 
-
-- The player will input their guess.
-
-- If the player guesses correctly, the app should display a message saying "Correct!". Then, after a pause of 3 seconds, it should randomly select the next set of emoji clues and display them to the player.
-
-- If the player’s guess is incorrect, the app should display a message saying “Incorrect! You have 2 more guesses remaining.”
-
-- If the player fails to guess correctly on the next two attempts, the app should display a message saying, `The film was <Film Name Here>!`. After a pause of 3 seconds, it should randomly select a new set of emoji clues and display them to the player.
-
-- When all films in the array have been used, the player should see a message saying "That's all folks!".
-
-- Each film should only be used once. There should be no repetition. 
-
-
-Stretch Goals
-
-- Use AI to decide if an answer is correct or incorrect. For example if the correct answer is "The Polar Express" but the player inputs "Polar Express" a straight comparison of the two strings will find that the player's answer was incorrect. AI could assess if there is sufficient similarity between the strings to judge it as correct. 
-
-- Improve the UX by disabling the form/button when the game is over and during the pause between questions.
-*/
-
 import { films } from '/data.js'
 
-// Some useful elements
-const guessInput = document.getElementById('guess-input')
-const messageContainer = document.getElementsByClassName('message-container')[0]
-const emojiCluesContainer = document.getElementsByClassName('emoji-clues-container')[0]
+let usedFilms = [];
+let currentFilm = null;
+let remainingGuesses = 3;
+let isWaiting = false;
+
+const form = document.getElementById('guess-form');
+const guessInput = document.getElementById('guess-input');
+const submitButton = document.querySelector('button[type="submit"]');
+const messageContainer = document.getElementsByClassName('message-container')[0];
+const emojiContainer = document.getElementsByClassName('emoji-clues-container')[0];
+
+function getRandomFilm() {
+  if (usedFilms.length === films.length) {
+    return null;
+  }
+
+  const availableFilms = films.filter(film => !usedFilms.includes(film));
+  const randomFilm = availableFilms[Math.floor(Math.random() * availableFilms.length)];
+  usedFilms.push(randomFilm);
+  return randomFilm;
+}
+
+function displayFilm(film) {
+  if (!film) {
+    endGame();
+    return;
+  }
+  emojiContainer.innerHTML = film.emoji.join("");
+}
+
+function endGame() {
+  emojiContainer.innerHTML = '';
+  messageContainer.innerHTML = "That's all folks!";
+  disableForm(true);
+}
+
+function displayMessage(message) {
+  messageContainer.innerHTML = message;
+}
+
+function disableForm(disable) {
+  guessInput.disabled = disable;
+  submitButton.disabled = disable;
+}
+
+function resetForNextQuestion() {
+  isWaiting = true;
+  disableForm(true);
+
+  setTimeout(() => {
+    currentFilm = getRandomFilm();
+    if (currentFilm) {
+      displayFilm(currentFilm);
+      remainingGuesses = 3;
+      guessInput.value = '';
+      messageContainer.innerHTML = '';
+      isWaiting = false;
+      disableForm(false);
+    } else {
+      endGame();
+    }
+  }, 3000);
+}
+
+function handleGuess(guess) {
+  if (guess.toLowerCase() === currentFilm.title.toLowerCase()) {
+    displayMessage('Correct!');
+    resetForNextQuestion();
+  } else {
+    remainingGuesses--;
+    if (remainingGuesses > 0) {
+      displayMessage(`Incorrect! You have ${remainingGuesses} more ${remainingGuesses === 1 ? 'guess' : 'guesses'} remaining.`);
+    } else {
+      displayMessage(`The film was ${currentFilm.title}!`);
+      resetForNextQuestion();
+    }
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (isWaiting) return;
+
+  const guess = guessInput.value.trim();
+  if (guess) {
+    handleGuess(guess);
+  }
+});
+
+currentFilm = getRandomFilm();
+if (currentFilm) {
+  displayFilm(currentFilm);
+} else {
+  endGame();
+}
