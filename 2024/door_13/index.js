9cd9d9e436d4513aa76dbd73268163ab10550260
Ariadne Engelbrecht <ariadneengelbrecht@dionysos.local>
2024-12-17 06:41:55 +0100

    Add solution to door 13.

--- 2024/door_13/index.js
+++ 2024/door_13/index.js
@@ -1,27 +1,87 @@
-// Santa needs your help to figure out if he has enough money to give everyone change!
-// Your goal will be to return true if everyone gets their correct change, and false if at least one person does not receive their correct change! Use the function below to get started!
+const santasPurse = new Map([
+  [5, 0],
+  [10, 0],
+  [20, 0]
+]);
 
-// Good luck and happy coding!!
 function correctChangeFromSanta(bills) {
-    
+  for (const bill of bills) {
+    if (bill === 5) {
+      santasPurse.set(5, santasPurse.get(5) + 1);
+    } else {
+      const change = bill - 5;
+      if (!canGiveChange(change)) {
+        return false;
+      }
+      santasPurse.set(bill, santasPurse.get(bill) + 1);
+    }
+  }
+  return true;
 }
 
-
-
-
-
-// You can leave this code as is, this will simply console.log() different text depending on if the test case returns true or false. Feel free to add additional test cases if you would like!
-
-// Should return true
-if (correctChangeFromSanta([5,5,5,10,20])) {
-    console.log("Nice job Santa, everyone got their correct change!")
-}else {
-    console.log("Looks like you have some work to do Santa, and bring some money next time!")
+function canGiveChange(change) {
+  if (change === 5) {
+    if (santasPurse.get(5) > 0) {
+      santasPurse.set(5, santasPurse.get(5) - 1);
+      return true;
+    }
+  } else if (change === 15) {
+    if (santasPurse.get(10) > 0 && santasPurse.get(5) > 0) {
+      santasPurse.set(10, santasPurse.get(10) - 1);
+      santasPurse.set(5, santasPurse.get(5) - 1);
+      return true;
+    }
+    if (santasPurse.get(5) >= 3) {
+      santasPurse.set(5, santasPurse.get(5) - 3);
+      return true;
+    }
+  }
+  return false;
 }
 
-// Should return false
-if (correctChangeFromSanta([5,5,10,10,20])) {
-    console.log("Nice job Santa, everyone got their correct change!")
-}else {
-    console.log("Looks like you have some work to do Santa, and bring some money next time!")
+function updatePurseStatus() {
+  const status = `$5: ${santasPurse.get(5)} | $10: ${santasPurse.get(10)} | $20: ${santasPurse.get(20)}`;
+  document.getElementById('purseStatus').textContent = status;
 }
+
+document.getElementById('billForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const billInput = document.getElementById('billInput');
+  const billValue = parseInt(billInput.value);
+
+  // Check if a valid number is selected
+  if (billValue) {
+    const successMessage = "Nice job Santa, everyone got their correct change!";
+    const failureMessage = "Looks like you have some work to do Santa, and bring some money next time!";
+
+    const messageBox = correctChangeFromSanta(billValue);
+    const messageBoxText = messageBox ? successMessage : failureMessage;
+    document.getElementById('message-box').textContent = messageBoxText;
+    updatePurseStatus();
+  } else {
+    // Clear the message if no valid option is selected
+    document.getElementById('message-box').textContent = '';
+  }
+
+  billInput.value = ''; // Reset the select element to the default option
+});
+
+document.getElementById('billForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const successMessage = "Nice job Santa, everyone got their correct change!";
+  const failureMessage = "Looks like you have some work to do Santa, and bring some money next time!";
+
+  const billInput = document.getElementById('billInput');
+  const bills = billInput.value.split(',').map(Number).filter(bill => [5, 10, 20].includes(bill));
+
+  const messageBox = correctChangeFromSanta(bills);
+  const messageBoxText = messageBox
+    ? successMessage : failureMessage;
+  document.getElementById('message-box').textContent = messageBoxText;
+  updatePurseStatus();
+  billInput.value = '';
+});
+
+updatePurseStatus();
